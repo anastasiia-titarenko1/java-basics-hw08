73c73e9511ef9fa56841da6a3be9858ab6f51e69
anastasiia-titarenko1 <titnastya92@gmail.com>
2021-04-01 14:37:17 +0300

    hm_05

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -11,6 +14,12 @@ public class RegExp {
      */
     public static String getIpAddress(String text) {
         // TODO fill in code here for extracting IP address using Regular Expression
+        String ipa = "[0-2]?[0-9]{0,2}[.][0-2]?[0-9]{0,2}[.][0-2]?([0-5]{0,2}|8)[.][0-2]?([0-9]{0,2})";
+        Pattern pattern = Pattern.compile(ipa);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            return matcher.group();
+        }
         return "";
     }
 }
